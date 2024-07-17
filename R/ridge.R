e7677892c8fc395fe4475269b1e53118a3daf23d
jonas7654 <veit.jonas@googlemail.com>
2024-07-17 11:24:19 +0200

    new

---
+++ R/ridge.R
@@ -0,0 +1,11 @@
+#' Estimates a ridge model
+#'
+#' @param x A number.
+#' @param y A number.
+#' @returns A numeric vector.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+
+ridge_estimator <- function()
+
